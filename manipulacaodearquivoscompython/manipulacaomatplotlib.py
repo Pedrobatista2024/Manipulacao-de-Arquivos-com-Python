7a601dcf981f718903eefaab0308e8ed56b60e21
Pedro Batista Neto <pedroplpellegrinni1918@gmail.com>
2025-07-23 15:01:44 -0300

    visualizacao_de_dados_com_matplotlib

--- manipulacaodearquivoscompython/manipulacaomatplotlib.py
+++ manipulacaodearquivoscompython/manipulacaomatplotlib.py
@@ -13,9 +13,42 @@ import matplotlib.pyplot as plt
 #plt.title('Teste Plot')
 #plt.show()
 
-x2 = [1,2,3,]
-y2 = [11,12,15]
+#x2 = [1,2,3,]
+#y2 = [11,12,15 ]
 
-plt.plot(x2, y2, label = 'Grafico com Matplotlib')
-plt.legend()
-plt.show()
+#plt.plot(x2, y2, label = 'Grafico com Matplotlib')
+#plt.legend()
+#plt.show()
+
+#Grafico de barras
+#x = [2,4,6,8,10]
+#y = [6,7,8,2,4]
+
+#plt.bar(x, y, label= 'Barras', color= 'green')
+#plt.legend()
+#plt.show()
+
+#x1 = [1,3,5,7,9]
+#y2 = [7,8,2,4,2]
+
+#plt.bar(x, y, label= 'Lista 1', color= 'green')
+#plt.bar(x1, y2, label= 'Lista 2', color= 'red')
+#plt.legend()
+#plt.show()
+
+#idades = [22,65,45,55,21,22,34,42,41,4,99,101,120,122,130,111,115,80,74,54,44,64,13,18,48]
+#ids = [x for x in range(len(idades))]
+#print(ids)
+
+#plt.bar(ids, idades)
+#plt.show()
+
+#Estograma
+
+#bins = [0,10,20,30,40,50,60,70,80,90,100,110,120,130]
+
+#plt.hist(idades, bins, histtype= 'bar', rwidth= 0.8)
+#plt.hist(idades, bins, histtype= 'stepfilled', rwidth= 0.8)
+#plt.show()
+
+

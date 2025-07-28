d1903508e3d2044e90b61cb156d19d79819d0acb
Pedro Batista Neto <pedroplpellegrinni1918@gmail.com>
2025-07-28 13:46:31 -0300

    visualizacao_de_graficos_com_seaborn

--- manipulacaodearquivoscompython/visualizacaoseaborn.py
+++ manipulacaodearquivoscompython/visualizacaoseaborn.py
@@ -38,12 +38,33 @@ df['peso']= random.sample(range(55,150), 30)
 
 #sea.clustermap(df)
 
+np.random.seed(42)
+n = 1000
+pct_smokers = 0.2
 
+flag_fumante = np.random.rand(n) < pct_smokers
+idade = np.random.normal(40,10,n)
+altura = np.random.normal(170,10,n)
+peso = np.random.normal(70,10,n)
 
+dados = pd.DataFrame({'altura':altura, 'peso':peso, 'flag_fumante':flag_fumante})
 
+dados['flag_fumante'] = dados['flag_fumante'].map({True:'fumante', False:'não fumante'})
 
+sea.set(style = 'ticks')
 
+sea.lmplot(x = 'altura', 
+           y = 'peso',
+           data = dados,
+           hue= 'flag_fumante', 
+           palette= ['tab:blue', 'tab:orange'], 
+           height= 7)
 
+plt.xlabel('altura (cm)')
+plt.ylabel('peso (kg)')
+plt.title('relação entre altura e peso de fumante e não fumantes')
+
+sea.despine()
 
 plt.show()
 

d2bcb4f25520dcf98c0b8e8d78b05a0baa152993
Pedro Batista Neto <pedroplpellegrinni1918@gmail.com>
2025-08-25 13:44:01 -0300

    pre_processamento_analise atemporal

--- manipulacaodearquivoscompython/analiseatemporal.py
+++ manipulacaodearquivoscompython/analiseatemporal.py
@@ -11,3 +11,29 @@ print(df_dsa.shape)
 print(df_dsa.head())
 
 print(df_dsa.tail())
+
+#pre_processamento do dados 
+
+#valor monimo da coluna data
+print(df_dsa.min())
+
+#valor maximo da coluna data
+print(df_dsa.max())
+
+print(df_dsa.info())
+#converte a coluna de data no tipo datetime
+df_dsa['Data'] = pd.to_datetime(df_dsa['Data'])
+
+print(df_dsa.head())
+
+print(df_dsa.info())
+
+serie_atemporal = df_dsa.set_index('Data')['Total_Vendas']
+
+print(type(serie_atemporal))
+
+print(serie_atemporal)
+
+serie_atemporal = serie_atemporal.asfreq('D')
+
+print(serie_atemporal)
